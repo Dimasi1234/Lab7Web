5e924644185211c4fd97a22ac31a3a50a5cc8d86
Dimas Galang Prasetyo <151460021+Dimasi1234@users.noreply.github.com>
2025-07-03 09:59:20 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,15 +6,25 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+// Halaman umum
+$routes->get('/', 'Page::home');
+$routes->get('/home', 'Page::home');
 $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
 $routes->get('/tos', 'Page::tos');
 
+// Artikel publik
 $routes->get('/artikel', 'Artikel::index');
 $routes->get('/artikel/(:any)', 'Artikel::view/$1');
 
-$routes->group('admin', function($routes) {
+// Login & Logout
+$routes->get('user/login', 'User::login');
+$routes->post('user/login', 'User::login');
+$routes->get('user/logout', 'User::logout');
+
+// Route admin dengan filter auth
+$routes->group('admin', ['filter' => 'auth'], function($routes) {
     $routes->get('artikel', 'Artikel::admin_index');
     $routes->add('artikel/add', 'Artikel::add');
     $routes->add('artikel/edit/(:any)', 'Artikel::edit/$1');
